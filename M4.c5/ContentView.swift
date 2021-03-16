f55a7596105cab298601c673a8935681fed424d8
Eiphoria <liuerbsb@gmal.com>
2021-03-16 11:15:13 +0900

    Initial Commit

---
+++ M4.c5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M4.c5
+//
+//  Created by LIU SHURUI on 2021/03/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M4.c5/M4_c5App.swift
@@ -0,0 +1,17 @@
+//
+//  M4_c5App.swift
+//  M4.c5
+//
+//  Created by LIU SHURUI on 2021/03/16.
+//
+
+import SwiftUI
+
+@main
+struct M4_c5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
